7f57b8923b40e6518df11e57ba21e4da48cd19f7
𝚩𝚪𝐘𝚫𝚴𝚻 𝚻𝚵𝐂𝚮 <botapi97@gmail.com>
2024-06-05 17:16:24 +0000

    Update Ping.js

--- commands/Ping.js
+++ commands/Ping.js
@@ -30,7 +30,7 @@ const { default: axios } = require('axios');
 france({ nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '🚨', 
+    reaction: '⚡', 
     fromMe: 'true', 
 
        
@@ -38,7 +38,7 @@ france({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*Pong!!*\n ```' + 2000 + '``` *ms*') 
+    return repondre('*Pong!!*\n ```' + 20000 + '``` *ms*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }

2507ceb5ae6810717005e3d920faf5943c40f0a9
𝚩𝚪𝐘𝚫𝚴𝚻 𝚻𝚵𝐂𝚮 <botapi97@gmail.com>
2024-06-06 22:00:52 +0000

    Update Ping.js

--- commands/Ping.js
+++ commands/Ping.js
@@ -38,7 +38,7 @@ france({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*Pong!!*\n ```' + 𝟮𝟬𝟬𝟬 + '``` *ms*') 
+    return repondre('*Pong!!*\n ```' +Itachi___ 𝟮𝟬𝟬𝟬 + '``` *ms*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
